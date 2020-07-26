dc11f6ada4762ae9d7a5e8a4c2dcd684c74e00ce
1994reka3 <rik3@re1kas-MacBookAir.local>
2020-07-27 03:16:59 +0900

    [Start]4-8

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'books#top'
+
+
+  post 'books' => 'books#create'
+
+  get 'books' => 'books#index'
+
+  post 'books' => 'books#create'
+
+  get 'books/:id' => 'books#show', as: 'books'
+
+
+end
+
