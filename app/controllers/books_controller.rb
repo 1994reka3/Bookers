07f82d85386dc875b458f14348de35a3bbb2371d
vagrant <vagrant@localhost.localdomain>
2020-07-27 13:07:13 +0000

    [Finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,18 +5,44 @@ class BooksController < ApplicationController
 
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice]='Book was successfully created.'
+      redirect_to book_path(@book)
+    else
+      @books = Book.all.order("id ASC")
+      render :index
+    end
   end
 
   def index
-  	@book = Book.new
-    @books = Book.all
+  @book = Book.new
+    @books = Book.all.order("id ASC")
   end
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]='Book was successfully updated.'
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice]='Book was successfully destroyed.'
+    redirect_to books_path
   end
 
 # ストロングパラメータ
